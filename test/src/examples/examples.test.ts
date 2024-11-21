2deea61a77985e37966e4b6fef535b46ec9bfd49
David Biró <dbiro97@gmail.com>
2024-11-21 15:29:43 +0100

    Add more tests

--- test/src/examples/examples.test.ts
+++ test/src/examples/examples.test.ts
@@ -1,4 +1,5 @@
 import { createCupleReactAdminAPI } from "@ra-api/index";
+import { createCupleReactAdminDataProvider } from "@ra-data-provider/index";
 import assert from "assert";
 import { describe, it } from "mocha";
 import { z } from "zod";
@@ -122,6 +123,87 @@ describe("Example", () => {
       });
 
       assert.equal(response2.result, "unexpected-error");
+    });
+  });
+
+  it("should accept custom builders", async () => {
+    const cs = await createClientAndServer((builder) => {
+      const authedBuilder = builder
+        .headersSchema(
+          z.object({
+            autherization: z.string(),
+          }),
+        )
+        .middleware(async ({ data }) => {
+          if (data.headers.autherization === "Bearer hey")
+            return { next: true as const, authData: { userId: 1 } };
+          return {
+            next: false as const,
+            statusCode: 403 as const,
+            result: "unauthorized-error" as const,
+          };
+        });
+      return {
+        admin: createCupleReactAdminAPI({
+          builder: authedBuilder,
+          resources: ["users", "posts"],
+          defaultHandlers: {
+            async create(data) {
+              throw new Error("unimplemented");
+            },
+            delete(data) {
+              throw new Error("unimplemented");
+            },
+            getList(data) {
+              throw new Error("unimplemented");
+            },
+            getMany(data) {
+              throw new Error("unimplemented");
+            },
+            getOne(data) {
+              throw new Error("unimplemented");
+            },
+            update(data) {
+              throw new Error("unimplemented");
+            },
+            updateMany(data) {
+              throw new Error("unimplemented");
+            },
+          },
+          overrides: {
+            users: {
+              async create(data) {
+                return success({
+                  item: { id: 1, name: data.body.data["name"] },
+                });
+              },
+            },
+          },
+        }),
+      };
+    });
+    await cs.run(async (client) => {
+      const authedClient = client.with(() => ({
+        headers: {
+          autherization: "foo",
+        },
+      }));
+
+      createCupleReactAdminDataProvider(authedClient.admin);
+
+      const response = await authedClient.admin.create.post({
+        query: {
+          resource: "users",
+        },
+        body: {
+          data: {
+            name: "Foo Bar",
+          },
+        },
+      });
+
+      if (response.result !== "success") throw new Error("response should be success");
+      assert.equal((response.item as { id: number; name: string })["name"], "Foo Bar");
     });
   });
 });
